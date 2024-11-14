e67acd110b1dfe4586dddbf190c66c02451bdd5f
AlexNdungu <alexmeta517@gmail.com>
2024-11-14 15:57:31 +0300

    Brand Filter Section

--- src/Layouts/FilterProductLayout.jsx
+++ src/Layouts/FilterProductLayout.jsx
@@ -1,10 +1,12 @@
 import styles from './filterProductLayout.module.css';
 import FilterSection from './Filter/FilterSection.jsx';
+import ProductSections from './Products/ProductsSection.jsx';
 
 function FilterProductLayout(){
     return(
         <div className={styles.lower_product_and_filter_section}>
             <FilterSection/>
+            <ProductSections/>
         </div>
     );
 }
---
+++ src/Layouts/Products/ProductsSection.jsx
@@ -0,0 +1,10 @@
+import styles from './productsSection.module.css';
+
+function ProductSections(){
+    return(
+        <div className={styles.products_section}>
+        </div>
+    );
+}
+
+export default ProductSections
