a41fe324c91935ee24b97a704019635c73b34578
AlexNdungu <alexmeta517@gmail.com>
2024-11-15 14:49:23 +0300

    single product component

--- src/Layouts/Products/ProductsSection.jsx
+++ src/Layouts/Products/ProductsSection.jsx
@@ -8,10 +8,10 @@ function ProductSections(){
                 <div className={styles.select_layout_sort_by_section}>
                     <div className={styles.select_layout_btns}>
                         <div className={styles.line_layout}>
-                            <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m21 12.7h-18v7.3c0 .62.519 1 1 1h16c.621 0 1-.52 1-1zm-18-1.4h18v-7.3c0-.478-.379-1-1-1h-16c-.478 0-1 .379-1 1z" fill-rule="nonzero"/></svg>
+                            <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m21 12.7h-18v7.3c0 .62.519 1 1 1h16c.621 0 1-.52 1-1zm-18-1.4h18v-7.3c0-.478-.379-1-1-1h-16c-.478 0-1 .379-1 1z" fillRule="nonzero"/></svg>
                         </div>
                         <div className={styles.grid_layout}>
-                            <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m8 16h-5v4c0 .621.52 1 1 1h4zm6.6 5v-5h-5.2v5zm6.4-5h-5v5h4c.478 0 1-.379 1-1zm0-1.4v-5.2h-5v5.2zm-18-5.2v5.2h5v-5.2zm11.6 0h-5.2v5.2h5.2zm1.4-6.4v5h5v-4c0-.478-.379-1-1-1zm-8 5v-5h-4c-.62 0-1 .519-1 1v4zm6.6-5h-5.2v5h5.2z" fill-rule="nonzero"/></svg>
+                            <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m8 16h-5v4c0 .621.52 1 1 1h4zm6.6 5v-5h-5.2v5zm6.4-5h-5v5h4c.478 0 1-.379 1-1zm0-1.4v-5.2h-5v5.2zm-18-5.2v5.2h5v-5.2zm11.6 0h-5.2v5.2h5.2zm1.4-6.4v5h5v-4c0-.478-.379-1-1-1zm-8 5v-5h-4c-.62 0-1 .519-1 1v4zm6.6-5h-5.2v5h5.2z" fillRule="nonzero"/></svg>
                         </div>
                     </div>
                     <div className={styles.sort_by_section}>
@@ -22,8 +22,6 @@ function ProductSections(){
                 </div>
             </div>
             <div className={styles.product_display_section}>
-                <SingleProduct/>
-                <SingleProduct/>
                 <SingleProduct/>
             </div>
         </div>
