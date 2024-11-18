0760135d48bcfc58e88d97de14568e4c3c59d4ab
AlexNdungu <alexmeta517@gmail.com>
2024-11-18 09:24:50 +0300

    Email and Password Input

--- src/Layouts/Auth/AuthLayout.jsx
+++ src/Layouts/Auth/AuthLayout.jsx
@@ -4,7 +4,10 @@ function AuthLogin(){
     return(
         <div className={styles.main_login_layout}>
             <div className={styles.log_lay_icon}></div>
-            <div className={styles.log_lay_form}></div>
+            <div className={styles.log_lay_form}>
+                <h2>Create An Account</h2>
+                <span>Enter Your Details And Start Selling Now.</span>
+            </div>
         </div>
     );
 }
