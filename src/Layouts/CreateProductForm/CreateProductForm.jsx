26911d3480de96698ee7425f6cd51eb28803ff5a
AlexNdungu <alexmeta517@gmail.com>
2024-12-16 12:54:17 +0300

    Create upload new product form

--- src/Layouts/CreateProductForm/CreateProductForm.jsx
+++ src/Layouts/CreateProductForm/CreateProductForm.jsx
@@ -1,8 +1,17 @@
 import styles from "./CreateProductForm.module.css"
+import Button from "../Auth/Btn/Button";
 
 function CreateProductForm(){
     return(
         <>
+            <div className={styles.createProductSection}>
+                <form action="">
+                    <input type="text" name="" id="" />
+                    <input type="number" name="" id="" />
+                    <textarea name="" id=""></textarea>
+                    <Button span="Create Product" />
+                </form>
+            </div>
         </>
     );
 }
