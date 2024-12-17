9cec1aec1c4ff061af21bd2d421c3a437d3104e0
AlexNdungu <alexmeta517@gmail.com>
2024-12-17 11:13:56 +0300

    connect to category endpoints

--- src/Layouts/CreateProductForm/CreateProductForm.jsx
+++ src/Layouts/CreateProductForm/CreateProductForm.jsx
@@ -27,12 +27,12 @@ function CreateProductForm(){
         setVisibility(!isVisible);
     };
 
-    const [data,setData] = useState(null);
-    const [error, setError] = useState(null);
+    const [categories,setCategories] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
             try{
                 const response = await axios.get("http://localhost:8080/api/category");
+                setCategories(response.data)
                 console.log(response.data);
             }catch(error){
                 console.log(error);
@@ -54,6 +54,11 @@ function CreateProductForm(){
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
                         </div>
                         <div className={styles.category_drop_down_component_section} style={{display: isVisible ? 'flex' : 'none'}} >
+                            {categories.map(category => 
+                                <div key={category.id} className={styles.drop_down_object}>
+                                    <span>{category.name}</span>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <input type="file" name="file" ref={fileInputRef} className={styles.upload_product_image} onChange={handleFile} />
